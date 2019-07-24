0a326b0d6bf490739b4478fc5038c29992e591b5
LiuPeng <33090734+Pen9uin@users.noreply.github.com>
2019-07-24 15:03:45 +0800

    Update 001 移除排序数组中的重复数字.cpp

--- 数组/001 移除排序数组中的重复数字.cpp
+++ 数组/001 移除排序数组中的重复数字.cpp
@@ -4,23 +4,15 @@
 
 class Solution {
 public:
-    int numDecodings(string s) {
-        int n=s.size();
-        if(!n)  return 0;
-        int *dp = new int[n];
-        if(s[0] != '0')     dp[0] = 1;
-        else    dp[0] = 0;
-        for(int i = 1; i < n; i++){
-            dp[i] = 0;
-            if(s[i] != '0')     dp[i] += dp[i-1];
-            int tmp = (s[i-1] - '0') * 10 + s[i] - '0';
-            if(tmp >= 10 && tmp <= 26){
-                if(i >= 2)  dp[i] += dp[i-2];
-                else    dp[i]++;
-            }
+    int removeDuplicates(vector<int>& nums) 
+    {
+        if (nums.empty()) return 0;
+        int index = 0;
+        for (int i = 1; i < nums.size(); i++) 
+        {
+            if (nums[index] != nums[i])
+                nums[++index] = nums[i];
         }
-        int res = dp[n-1];
-        delete [] dp;
-        return res;
+        return index + 1;
     }
 };
