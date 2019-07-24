4e9c4ec8336b0b660b92a1aee77a4ee744261811
LiuPeng <33090734+Pen9uin@users.noreply.github.com>
2019-07-24 15:01:20 +0800

    Update 001 移除排序数组中的重复数字.cpp

--- 数组/001 移除排序数组中的重复数字.cpp
+++ 数组/001 移除排序数组中的重复数字.cpp
@@ -1,19 +1,26 @@
 
-```C++
 // LeetCode, Remove Duplicates from Sorted Array
 // 时间复杂度O(n),空间复杂度O(1)
+
 class Solution {
 public:
-	int removeDuplicates(vector<int>& nums) 
-	{
-		if (nums.empty()) return 0;
-		int index = 0;
-    	for (int i = 1; i < nums.size(); i++) 
-    	{
-			if (nums[index] != nums[i])
-				nums[++index] = nums[i];
-    	}
-    	return index + 1;
-	}
+    int numDecodings(string s) {
+        int n=s.size();
+        if(!n)  return 0;
+        int *dp = new int[n];
+        if(s[0] != '0')     dp[0] = 1;
+        else    dp[0] = 0;
+        for(int i = 1; i < n; i++){
+            dp[i] = 0;
+            if(s[i] != '0')     dp[i] += dp[i-1];
+            int tmp = (s[i-1] - '0') * 10 + s[i] - '0';
+            if(tmp >= 10 && tmp <= 26){
+                if(i >= 2)  dp[i] += dp[i-2];
+                else    dp[i]++;
+            }
+        }
+        int res = dp[n-1];
+        delete [] dp;
+        return res;
+    }
 };
-```
